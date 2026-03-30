56db05a4cc928a598b38f48592d1e4720859066c
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2026-03-30 22:44:12 +0800

    feat(search): add a small logo

--- search-website/src/components/SearchItem.vue
+++ search-website/src/components/SearchItem.vue
@@ -10,7 +10,7 @@ const createdAt = new Date(props.data.created_at)
 </script>
 <template>
   <div class="flex-col p-2 bg-gray-200 border border-gray-400 rounded-xl m-2">
-    <div class="text-sm text-gray-400">
+    <div class="text-sm text-gray-400 overflow-x-hidden">
       {{ breadcumbs }}
     </div>
     <a :href="props.data.url" class="text-xl text-blue-600">{{ title }}</a>
